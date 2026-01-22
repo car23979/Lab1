13d26c33bdac4e902a82e28ec9152ab6fb40d4e3
car23979 <125841463+car23979@users.noreply.github.com>
2026-01-22 01:14:49 -0600

    Definición de display
    Definición de pines de display

--- Lab1 Digital 2/Lab1 Digital 2/display7seg.h
+++ Lab1 Digital 2/Lab1 Digital 2/display7seg.h
@@ -9,7 +9,16 @@
 #ifndef DISPLAY7SEG_H_
 #define DISPLAY7SEG_H_
 
+#include "avr/io.h"
 
+// Definiciones de pines de display ánodo común
+#define  SEG_A		PIND6	// D6
+#define  SEG_B		PIND5	// D5
+#define  SEG_C		PIND2	// D2
+#define  SEG_D		PIND3	// D3
+#define  SEG_E		PIND4	// D4
+#define  SEG_F		PIND5	// D7
+#define  SEG_G		PINB0	// D8
 
 
 
