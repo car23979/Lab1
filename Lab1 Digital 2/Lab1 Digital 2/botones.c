e0974c3d58500b67eb8d8e23dd1a924cf23cafca
car23979 <125841463+car23979@users.noreply.github.com>
2026-01-22 01:53:14 -0600

    Anti-rebote
    Estructura para manejo de botón de anti-rebote, se detectan los flancos

--- Lab1 Digital 2/Lab1 Digital 2/botones.c
+++ Lab1 Digital 2/Lab1 Digital 2/botones.c
@@ -4,3 +4,15 @@
  * Created: 19/01/2026 01:07:32 a. m.
  *  Author: David Carranza
  */ 
+
+#include "botones.h"
+#include <avr/io.h>
+
+// Estructura para manejo de botón con anti-rebote
+typedef struct {
+	uint8_t pin;				// Pin del botón
+	uint8_t estado_anterior;	// Estado anterior (detección de flanco)
+	uint8_t estado_actual;		// Estado actual (filtrado)
+	uint8_t presionado_flag;	// Bandera de botón presionado
+	uint8_t ultimo_cambio;		// Tiempo del último cambio
+	} Boton;
