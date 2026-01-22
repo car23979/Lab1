fc300401e028c575cc32757faa26528758cd5fb3
car23979 <125841463+car23979@users.noreply.github.com>
2026-01-22 02:24:20 -0600

    Jugador 2
    Se definen los pines para las leds del jugador 2

--- Lab1 Digital 2/Lab1 Digital 2/main.c
+++ Lab1 Digital 2/Lab1 Digital 2/main.c
@@ -18,3 +18,9 @@
 #define J1_LED1		PINB2	// D10	- bit 1
 #define J1_LED2		PINB3	// D11	- bit 2
 #define J1_LED3		PINB4	// D12	- bit 3
+
+// LEDs Jugador 2 - PORTB y PORTC (4 leds para contador binario)
+#define J2_LED0		PINB5	// D13	- bit 0
+#define J2_LED1		PINC0	// A0	- bit 1
+#define J2_LED2		PINC1	// A1	- bit 2
+#define J2_LED3		PINC2	// A2	- bit 3
