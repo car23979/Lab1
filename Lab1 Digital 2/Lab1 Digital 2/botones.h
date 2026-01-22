1465b8efd91067f9e2df46208765517a14ddeea9
car23979 <125841463+car23979@users.noreply.github.com>
2026-01-22 01:49:08 -0600

    Definición y funciones
    Definición de pines de botones en el PINC, y los prototipos de funciones para cuando los botones se presionan

--- Lab1 Digital 2/Lab1 Digital 2/botones.h
+++ Lab1 Digital 2/Lab1 Digital 2/botones.h
@@ -9,8 +9,18 @@
 #ifndef BOTONES_H_
 #define BOTONES_H_
 
+#include <stdint.h>
 
+// Definiciones de pines de botones
+#define BTN_INICIO		PINC3	// A3
+#define BTN_J1			PINC4	// A4
+#define BTN_J2			PINC5	// A5
 
-
+// Prototipos de funciones
+void botones_init(void);
+void botones_actualizar(uint32_t tiempo_actual);
+uint8_t boton_inicio_presionado(void);
+uint8_t boton_j1_presionado(void);
+uint8_t boton_j2_presionado(void);
 
 #endif /* BOTONES_H_ */
