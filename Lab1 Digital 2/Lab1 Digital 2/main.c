5d117acddc461bbe51682b0d9fe49d8d72808f62
car23979 <125841463+car23979@users.noreply.github.com>
2026-01-22 02:22:09 -0600

    Contador J1
    Se definen los pines para el contador del jugador 1

--- Lab1 Digital 2/Lab1 Digital 2/main.c
+++ Lab1 Digital 2/Lab1 Digital 2/main.c
@@ -12,12 +12,9 @@
 #include "display7seg.h"
 #include "botones.h"
 
-
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
-    }
-}
-
+// Configuración de puertos
+// LEDs Jugador 1 - PORTB (4 leds para contador binario)
+#define J1_LED0		PINB1	// D9	- bit 0
+#define J1_LED1		PINB2	// D10	- bit 1
+#define J1_LED2		PINB3	// D11	- bit 2
+#define J1_LED3		PINB4	// D12	- bit 3
